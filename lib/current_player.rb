4d75a7f52d36d053ae97a8a85ad23ad5b2c64997
Sanjeev Tamang <sanzeevtamang@gmail.com>
2019-06-28 04:52:08 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -12,5 +12,9 @@ def turn_count(board)
 end
 
 def current_player(board)
-  turn_count(board) % 2 != 0 ? return "O" : return "X"
+  if turn_count(board) % 2 != 0 
+    return "O" 
+  else
+    return "X"
+  end
 end
